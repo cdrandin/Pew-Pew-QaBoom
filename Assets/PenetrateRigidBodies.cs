f644f4dd975d5fcc4b74160ba0729b2ca8b11362
cdrandin <cdrandin@hotmail.com>
2014-08-21 18:19:46 -0700

    Set prefabs for cube tiling. Set force appropriately
    Prefabs are created to be able to modify all the subset tiles in larger
    aspect. Carefully. adjust forces in play and set rigidbodies to get the
    right feeling. Currently, works for the small scope. Need to check for
    how the impact from a great distance reacts to hitting the cubes.

---
+++ Assets/PenetrateRigidBodies.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PenetrateRigidBodies : MonoBehaviour 
+{
+	public float force;
+
+	void Start()
+	{
+	}
+
+	void Update()
+	{
+		Vector3 mousePos = Input.mousePosition;
+		mousePos.z = Mathf.Abs(0.0f - Camera.main.transform.position.z);
+		mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			rigidbody.AddForce((mousePos - transform.position).normalized * force);
+		}
+	}
+
+	// Rigidbody works best when mass is 100kg
+	void OnCollisionExit(Collision collision)
+	{
+		Debug.Log(Vector3.SqrMagnitude(rigidbody.velocity));
+		float amount = Vector3.SqrMagnitude(rigidbody.velocity);
+		if(collision.gameObject.layer == LayerMask.NameToLayer("Dynamic Objects") && (amount > .1f))
+		{
+			Destroy(collision.gameObject);
+		}
+
+		if(amount <= .1f)
+		{
+			rigidbody.velocity = Vector3.zero;
+		}
+	}
+}
